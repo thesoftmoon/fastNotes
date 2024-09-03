38094defbbd5398fb016af90e26efa8b369de623
thesoftmoon <tomas.pacheco.c@gmail.com>
2024-09-03 17:34:13 -0400

    storage service add

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageService {
+  //_ al principio se usa para referirse a una propiedad privada
+  private _storage: Storage | null = null;
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  // init the angular storage
+  async init() {
+    this._storage = await this.storage.create();
+  }
+
+  //@param key
+  //@returns storaged value {string}
+  public set(key: string, value: any) {
+    this._storage?.get(key);
+  }
+
+  //@param key
+  //Remove item from storage based on key (name)
+  public get(key: string): Promise<any> | undefined {
+    return this._storage?.get(key);
+  }
+
+  //param key
+  //Remove item from storage based on key (name)
+  public remove(key: string) {
+    this._storage?.remove(key);
+  }
+
+  // clear all from storage
+  public clear() {
+    this._storage?.clear();
+  }
+}
